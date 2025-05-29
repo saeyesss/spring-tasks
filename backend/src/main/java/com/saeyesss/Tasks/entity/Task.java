39a3404c13c9139094e3f859f00d60a37c4f0d72
saeyesss <shreyasnidugala@gmail.com>
2025-05-29 11:23:20 +0530

    add entity

--- backend/src/main/java/com/saeyesss/Tasks/entity/Task.java
+++ backend/src/main/java/com/saeyesss/Tasks/entity/Task.java
@@ -1,40 +1,44 @@
 package com.saeyesss.Tasks.entity;
 
-import com.saeyesss.Tasks.enums.Role;
+import com.saeyesss.Tasks.enums.Priority;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Table(name = "tasks)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Title cannot be blank")
+    private String title;
 
-    @Column(unique = true, nullable = false)
-    private String userName;
-
-    @Column(nullable = false)
-    @NotBlank(message = "Password is required")
-    private String password;
+    private String description;
+    @NotNull
+    private Boolean completed;
 
     @Enumerated(EnumType.STRING)
-    private Role role;
+    private Priority priority;
 
+    private LocalDateTime dueDate;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Task> tasks = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private
+
+
 }
